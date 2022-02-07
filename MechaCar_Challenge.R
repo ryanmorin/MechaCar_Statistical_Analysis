677461e72ead2770c5271b786f7f354b1fe34c34
Ryan Morin <ryanjmorin@gmail.com>
2022-02-07 14:12:19 -0800

    part two of assignment

--- MechaCar_Challenge.R
+++ MechaCar_Challenge.R
@@ -6,3 +6,9 @@ class(MechaCar_data) #confirms that imported as dataframe
 fit <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle 
           + ground_clearance + AWD, data=MechaCar_data)
 summary(fit) # show results
+
+#Total summary data frame with the suspension_data
+total_summary <- Suspension_data %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),Variance_PSI=var(PSI),Std_Dev_PSI=sd(PSI)) #create summary table with multiple columns
+
+#Lot summary dataframe with the suspension data
+lot_summary <- Suspension_data %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),Variance_PSI=var(PSI),Std_Dev_PSI=sd(PSI)) #create summary table with multiple columns
